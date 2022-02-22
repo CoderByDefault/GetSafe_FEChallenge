966d57ea3169f4db24a0919464c1baa9c15ab568
CoderByDefault <gul@mail.uni-paderborn.de>
2022-02-22 20:28:59 +0100

    initial commit

--- src/buyflow/Buyflow.tsx
+++ src/buyflow/Buyflow.tsx
@@ -1,42 +1,45 @@
-import React, { useState } from 'react'
-import AgeStep from './AgeStep'
-import EmailStep from './EmailStep'
-import SummaryStep from './SummaryStep'
+import React, { useState } from "react";
+import AgeStep from "./AgeStep";
+import EmailStep from "./EmailStep";
+import SummaryStep from "./SummaryStep";
 
 interface BuyflowProps {
-  productId: ProductIds
+  productId: ProductIds;
 }
 
 export enum ProductIds {
-  devIns = 'dev_ins',
+  devIns = "dev_ins",
+  desIns = "des_ins"
 }
 
 const PRODUCT_IDS_TO_NAMES = {
-  [ProductIds.devIns]: 'Developer Insurance',
-}
+  [ProductIds.devIns]: "Developer Insurance",
+  [ProductIds.desIns]: "Designer Insurance"
+};
 
 const Buyflow: React.FC<BuyflowProps> = (props) => {
-  const [currentStep, setStep] = useState('email')
+  const [currentStep, setStep] = useState("email");
   const [collectedData, updateData] = useState({
-    email: '',
+    email: "",
     age: 0,
-  })
+    name: ""
+  });
   const getStepCallback = (nextStep: string) => (field: string, value: any) => {
-    updateData({ ...collectedData, [field]: value })
-    setStep(nextStep)
-  }
+    updateData({ ...collectedData, [field]: value });
+    setStep(nextStep);
+  };
   return (
     <>
       <h4>Buying {PRODUCT_IDS_TO_NAMES[props.productId]}</h4>
-      {(currentStep === 'email' && <EmailStep cb={getStepCallback('age')} />) ||
-        (currentStep === 'age' && (
-          <AgeStep cb={getStepCallback('summary')} />
+      {(currentStep === "email" && <EmailStep cb={getStepCallback("age")} />) ||
+        (currentStep === "age" && (
+          <AgeStep cb={getStepCallback("summary")} />
         )) ||
-        (currentStep === 'summary' && (
+        (currentStep === "summary" && (
           <SummaryStep collectedData={collectedData} />
         ))}
     </>
-  )
-}
+  );
+};
 
-export default Buyflow
+export default Buyflow;
